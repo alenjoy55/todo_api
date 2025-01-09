32339af32b0bb246ee8e62b0a5e6172351641e5b
alenjoy55 <alen85292@gmail.com>
2025-01-09 13:27:30 +0530

    update

--- todo_frontend/src/Components/List.jsx
+++ todo_frontend/src/Components/List.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+// import { error } from "console"
 function List(){
     const [data,setData]=useState([])
-    const [formData,setFormData]=useState({task:'',description:''})
     const [editing,setEditing] = useState(false)
     const [editdata,setEditData] = useState(null)
     useEffect(()=>{
@@ -15,20 +15,14 @@ function List(){
         setEditing(true)
         setEditData(task)
     }
-    const handleChange = (e)=>{
-        // console.log(e.target);
-        const {task,value}=e.target
-        setFormData((prv)=>({...prv,[task]:value}))
-        // console.log(formData);
-        
-      }
-      const handleSubmit = (e)=>{
-        e.preventDefault()
-        setData((prv)=>[...prv,formData])
-        setFormData({task:'',description:''})
-        // console.log(data);
-        
-      }
+    const updateDtls =(id,task)=>{
+        setEditing(false)
+        axios.put(`http://127.0.0.1:8000/api/todo/${id}/`,task).then(res=>{
+            setData(data.map((prv)=>prv.id==id ? res.data : prv))
+        }).catch(error =>console.log(error.message)
+        )
+    }
+    
     
     return(
         <div className="container">
@@ -51,15 +45,23 @@ function List(){
                 ))}
                 </tbody>
             </table>
-            {editing ? <EditForm curTask={editdata}/>:null}
+            {editing ? <EditForm curTask={editdata} updatefun={updateDtls} />:null}
         </div>
     )
 }
 const EditForm =({curTask})=>{
     const [task,setTask] = useState(curTask)
+    const handleChange = (e)=>{
+        const {name,value}=e.target
+       setTask({...task,[name]:value})  
+    }
+      const handleSubmit = (e)=>{
+        e.preventDefault()
+        updatefun(task.id,task)
+    }
     return(
         <form onSubmit={handleSubmit}>
-            <input type="text" name="title" id="title" value={task.task}  onChange={handleChange}/>
+            <input type="text" name="task" id="titile" value={task.task}  onChange={handleChange}/>
             <input type="text" name="description" id="description" value={task.description} onChange={handleChange}/>
             <input type="submit" value="Update" />
         </form>
